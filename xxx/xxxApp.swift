0c7e635ae0bf653b64fe2bf8a0104dc0d493395f
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2022-10-12 16:08:32 +0700

    Initial Commit

---
+++ xxx/xxxApp.swift
@@ -0,0 +1,17 @@
+//
+//  xxxApp.swift
+//  xxx
+//
+//  Created by Chalermchon COJ on 12/10/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct xxxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
